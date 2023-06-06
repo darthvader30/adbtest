6a6c10c38c83357e7ca090ea90c1fb2802c1864f
darthvader30 <singhkaran9830@gmail.com>
2023-06-06 01:03:14 -0500

    no idea

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ username = "kxs5434"
 password = "root@123"
 conn= pyodbc.connect('DRIVER='+driver+';SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = conn.cursor() #cursor 
-cursor.execute('''Select * from all_day''')
-check_username=cursor.fetchall()
-print(check_username)
+cursor.execute('''Select * from dbo.all_day''')
+query_output=cursor.fetchall()
+print(query_output)
 
 @app.route("/")
 def hello_world():
@@ -21,5 +21,5 @@ def hello_world():
     # names = c1.fetchall()
     # except Exception as e:
     # print(e)
-    # return check_username
+    # return query_output
     return "<p>Hello, World!</p>"
